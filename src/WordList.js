1b04abad06f090ccd3af52f7448cb055cad7c06d
Selena Groh <selena.groh@gmail.com>
2022-03-09 01:57:34 -0500

    Fix plural

--- src/WordList.js
+++ src/WordList.js
@@ -3,8 +3,8 @@ import "./WordList.scss";
 const WordList = ({ words }) => (
   <div className="WordList">
     <p className="WordList-wordCount">
-      You have found {words.length} words. (But no guarantees they're real words
-      -- yet!)
+      You have found {words.length} word{words.length === 1 ? "" : "s"}. (But no
+      guarantees they're real words -- yet!)
     </p>
     <ul>
       {words.map((word) => (
